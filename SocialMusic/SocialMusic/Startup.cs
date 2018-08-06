ac599b7db99d8d1c68992fee839f1ea892770898
NickMadworld <brian-kio@hotmail.com>
2018-08-06 16:06:11 -0500

    Init

---
+++ SocialMusic/SocialMusic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialMusic.Startup))]
+namespace SocialMusic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
